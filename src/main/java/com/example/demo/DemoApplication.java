eb497641401498cddea78b34eb3c93eecf6bb8bd
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2024-06-11 09:53:14 +0200

    chore: add kill command

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,43 +15,47 @@ import org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProc
 @EnableScheduling
 public class DemoApplication implements CommandLineRunner {
 
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Joining thread, you can press Ctrl+C to shutdown application");
+        Thread.currentThread().join();
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Joining thread, you can press Ctrl+C to shutdown application");
-		Thread.currentThread().join();
-	}
+    @Bean
+    ThreadPoolTaskSchedulerCustomizer threadPoolTaskSchedulerPhaseCustomizer() {
+        return p -> {
+            p.setPhase(0);
+            p.setContinueExistingPeriodicTasksAfterShutdownPolicy(true);
+            p.setWaitForTasksToCompleteOnShutdown(true);
+            p.setAcceptTasksAfterContextClose(true);
+            p.setExecuteExistingDelayedTasksAfterShutdownPolicy(true);
+            p.setAwaitTerminationSeconds(180);
+            p.setPoolSize(4);
+            p.setThreadNamePrefix("custom-scheduler-");
+        };
+    }
 
-	@Bean
-	ThreadPoolTaskSchedulerCustomizer threadPoolTaskSchedulerPhaseCustomizer() {
-		return p -> {
-			p.setPhase(0);
-			p.setContinueExistingPeriodicTasksAfterShutdownPolicy(true);
-			p.setWaitForTasksToCompleteOnShutdown(true);
-			p.setAcceptTasksAfterContextClose(true);
-			p.setExecuteExistingDelayedTasksAfterShutdownPolicy(true);
-			p.setAwaitTerminationSeconds(180);
-		};
-	}
+    @Bean(name = ScheduledAnnotationBeanPostProcessor.DEFAULT_TASK_SCHEDULER_BEAN_NAME)
+    TaskScheduler taskScheduler(ThreadPoolTaskSchedulerBuilder builder) {
+        //This is the default task scheduler used by ScheduledAnnotationBeanPostProcessor, we use this to prevent "No TaskScheduler/ScheduledExecutorService bean found for scheduled processing"
+        return builder
 
-	@Bean(name = ScheduledAnnotationBeanPostProcessor.DEFAULT_TASK_SCHEDULER_BEAN_NAME)
-	TaskScheduler taskScheduler(ThreadPoolTaskSchedulerBuilder builder) {
-		//This is the default task scheduler used by ScheduledAnnotationBeanPostProcessor, we use this to prevent "No TaskScheduler/ScheduledExecutorService bean found for scheduled processing"
-		return builder.build();
-	}
+                .build();
+    }
 
-	@Scheduled(fixedRate = 1000)
-	public void test() {
-		var pid = System.getProperty("PID");
-		System.out.println("Kill me with 'kill -int "+ pid + "' - " + System.currentTimeMillis());
-	}
+    @Scheduled(fixedRate = 1000, scheduler = ScheduledAnnotationBeanPostProcessor.DEFAULT_TASK_SCHEDULER_BEAN_NAME)
+    public void test() {
 
-	@Bean
-	WaitOnShutdown waitOnShutdown() {
-		return new WaitOnShutdown();
-	}
+        var pid = System.getProperty("PID");
+        System.out.println("Kill me with 'kill -int " + pid + "' - " + Thread.currentThread().getName() + " - " + System.currentTimeMillis());
+    }
+
+    @Bean
+    WaitOnShutdown waitOnShutdown() {
+        return new WaitOnShutdown();
+    }
 }
